8b0ecd4c4d155bb29192471328453356d1de4bd0
Chandan-dev <64628111+ChandanKumar-git@users.noreply.github.com>
2023-10-22 07:23:10 +0000

    Data_Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" //treat all JS code as newer version
+
+//alert("hello") we are using nodejs, not browser
+
+console.log(3+
+    3); // code readability should be high
+
+console.log("Chandan");
+
+let name="chandan";
+let age= 26;
+let isLogedIn =false;
+
+// Some data types
+// 1. number range -->only safely store integers in the range -(253 − 1) ( Number.MIN_SAFE_INTEGER ) to 253 − 1  Number.
+
+// 2.bigintg not much use
+// 3.string=""
+// 4.boolean==>true/false
+// 5.null==>standalone value
+// 6.undefine==>valuue is not assign
+// 7.symbol==>it mostly use in react, for finding uniquines, like figmatool use symbole
+
+//  these all above are premitive dattatypes
+
+// Object
+
+console.log(typeof age)
+console.log(typeof name)
+
+console.log(typeof null) //type of null is object
+console.log(typeof undefined) //type of undefined is undefined.
