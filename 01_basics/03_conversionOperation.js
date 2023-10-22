a4a97ea7cb5594eabdd08607087db5b7e2624076
Chandan-dev <64628111+ChandanKumar-git@users.noreply.github.com>
2023-10-22 08:12:45 +0000

    03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,91 @@
+// let score="33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// let score="33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //OUTPUT==NAN
+
+// let score=null
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//OUTPUT==0
+
+// let score="33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//NAN
+
+let score=true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//  op=1
+
+let isLogedIn=1
+
+let booleanIsLoggedIn=Boolean(isLogedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"chandan"=>true
+
+// let sommeNumber=33
+
+// let stringNumber=String(someNumber)
+
+// console.log(stringNumber)
+
+
+
+//*Operations *//
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(3*4)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="hello"
+let str2=" Chandan"
+
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+
+// console.log(+true) //1
+// console.log(true+)//error
+
+// console.log(+"");//0
+
